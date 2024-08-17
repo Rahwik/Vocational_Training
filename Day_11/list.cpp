4b8ab00d6c7dad2ac2c0964a06c34d45c8b2784d
Rahul <rahul.3057.12@outlook.com>
2024-08-17 15:24:23 +0530

    list in stl

--- Day_11/list.cpp
+++ Day_11/list.cpp
@@ -3,13 +3,69 @@
 
 using namespace std;
 
-int main() {
-    list<int> l;
-    l.push_back(1);
-    l.push_back(2);
+int main()
+{
+    list<int> a;
 
-    for (auto i : l) {
-        cout << i << " ";
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+
+    cout << "Initial list: ";
+    for (auto x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    a.push_front(0);
+
+    cout << "After push_front: ";
+    for (auto x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    auto b = a.begin();
+    b++;
+    a.erase(b);
+    cout << "After erase: ";
+    for (auto x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    a.remove(2);
+    cout << "After remove: ";
+    for (auto x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    a.sort();
+    cout << "After sort: ";
+    for (auto x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    
+    a.pop_front();
+    cout << "After pop_front: ";
+    for (auto x : a)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    a.pop_back();
+    cout << "After pop_back: ";
+    for (auto x : a)
+    {
+        cout << x << " ";
     }
     cout << endl;
 
